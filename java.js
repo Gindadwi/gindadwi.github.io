a8833eb17e6f85fd7875da3c473ded3177ea6d5a
Gindadwi <gindadwi144@gmail.com>
2024-03-28 21:07:58 +0700

    none

--- java.js
+++ java.js
@@ -1,17 +1,4 @@
 
-    function sendMessage() {
-        var phonenumber = "+6281228900185"
-        var name = document.querySelector("name").value;
-        var email = document.querySelector("email").value;
-        var Message = document.querySelector("Message").value;
-
-       var url = "https://wa.me/" + phonenumber + "?text="
-       +"Name: "  +name+ "%0a"
-       +"Email: " +email+ "%0a"
-       +"Message: " +Message+ "%0a%0a"
-
-       window.open(url, '_blank').focus();
-    }
 
 
 
