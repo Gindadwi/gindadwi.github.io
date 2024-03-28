a381287251b3163b99ca7982bcee601f0c003abd
Gindadwi <gindadwi144@gmail.com>
2024-03-28 20:42:12 +0700

    pembaruan form 123

--- java.js
+++ java.js
@@ -1,31 +1,15 @@
-// Fungsi untuk mengirim pesan WhatsApp
-    // function sendMessage() {
-    //     const name = document.getElementById("name").value;
-    //     const email = document.getElementById("email").value;
-    //     const Message = document.getElementById("Message").value;
 
-    //     const url = "https://api.whatsapp.com/send?phone=6281228900185&text=%0Aassalamualaikum.%0A%0Anama%20saya%20* " +name+ " *%0Aemail%20*"+email+"*%0A%0A*"+Message+"*"
-    //     window.open(url);
-    // }
+    function sendMessage() {
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const Message = document.getElementById("Message").value;
+
+        const url = "https://api.whatsapp.com/send?phone=6281228900185&text=%0Aassalamualaikum.%0A%0Anama%20saya%20* " +name+ " *%0Aemail%20*"+email+"*%0A%0A*"+Message+"*"
+        window.open(url);
+    }
 
 
-function sendMessage() {
-    const phoneNumber = "6281228900185"; // Nomor WhatsApp penerima
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("Message").value;
 
-    // Format pesan sesuai dengan aturan WhatsApp
-    const formattedMessage = encodeURIComponent(
-        `Assalamualaikum. Nama saya *${name} *Email*${email}* *${message}*`
-    );
-
-    // Buat URL dengan nomor telepon dan pesan yang diformat
-    const url = `https://wa.me/${phoneNumber}?text=${formattedMessage}`;
-
-    // Buka URL untuk membuka aplikasi WhatsApp pada perangkat seluler
-    window.open(url, "_blank");
-}
 
 
 
