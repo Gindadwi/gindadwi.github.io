8afbaec7d38c0ea2d79943d0feeef1cbbbbc8d60
Gindadwi <gindadwi144@gmail.com>
2024-03-28 21:30:39 +0700

    form2 dan form

--- java.js
+++ java.js
@@ -1,13 +1,10 @@
 function sendMessage() {
-    var phonenumber = "+6281228900185";
+    var phonenumber = "6281228900185";
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-    var Message = document.getElementById("Message").value;
+    var message = document.getElementById("Message").value;
 
-    var url = "https://wa.link/me/" + phonenumber + "?text="
-        + "Name: " + name + "%0a"
-        + "Email: " + email + "%0a"
-        + "Message: " + Message + "%0a%0a";
+    var url = "https://api.whatsapp.com/send?phone=" + phonenumber + "&text=Assalamualaikum.%0A%0ANama:%20" + encodeURIComponent(name) + "%0AEmail:%20" + encodeURIComponent(email) + "%0A%0A" + encodeURIComponent(message);
 
-    window.open(url, '_blank').focus();
+    window.open(url, '_blank');
 }
