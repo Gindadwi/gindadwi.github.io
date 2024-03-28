e9b4da25e465120e69e46361dbc9a83b3f49c043
Gindadwi <gindadwi144@gmail.com>
2024-03-28 21:26:46 +0700

    form2 dan form

--- java.js
+++ java.js
@@ -1,22 +1,13 @@
+function sendMessage() {
+    var phonenumber = "+6281228900185";
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var Message = document.getElementById("Message").value;
 
-// direct wa
-    function sendMessage() {
-        var phonenumber = "+6281228900185"
-        var name = document.getElementById("name").value;
-        var email = document.getElementById("email").value;
-        var Message = document.getElementById("Message").value;
+    var url = "https://wa.link/me/" + phonenumber + "?text="
+        + "Name: " + name + "%0a"
+        + "Email: " + email + "%0a"
+        + "Message: " + Message + "%0a%0a";
 
-       var url = "https://wa.link.me/" + phonenumber + "?text="
-       +"Name: "  +name+ "%0a"
-       +"Email: " +email+ "%0a"
-       +"Message: " +Message+ "%0a%0a"
-
-       window.open(url, '_blank').focus();
-    }
-
-
-
-
-
-
-    
+    window.open(url, '_blank').focus();
+}
