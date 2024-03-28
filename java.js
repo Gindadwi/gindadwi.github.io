24cbde47f58bd2d477eae0e96f0629dbb1aeb28f
Gindadwi <gindadwi144@gmail.com>
2024-03-28 21:24:48 +0700

    form2

--- java.js
+++ java.js
@@ -2,9 +2,9 @@
 // direct wa
     function sendMessage() {
         var phonenumber = "+6281228900185"
-        var name = document.querySelector("name").value;
-        var email = document.querySelector("email").value;
-        var Message = document.querySelector("Message").value;
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var Message = document.getElementById("Message").value;
 
        var url = "https://wa.link.me/" + phonenumber + "?text="
        +"Name: "  +name+ "%0a"
