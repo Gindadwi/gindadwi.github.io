21142a089b7ad66bf619ffaa51f120da790bf1b1
Gindadwi <gindadwi144@gmail.com>
2024-04-01 11:38:24 +0700

    menambahkan pop up di setiap button

--- java.js
+++ java.js
@@ -20,10 +20,35 @@ $(document).ready(function () {
         }
     });
 });
+
 $(document).ready(function () {
     $('#DownloadCV').click(function () {
         // Memeriksa apakah ada input yang kosong
          swal('', 'Maaf Link CV Belum Saya cantumkan', 'warning'); // Menampilkan peringatan menggunakan sweetalert
+        return false; // Menghentikan pengiriman form jika ada input yang kosong
+       
+    });
+});
+$(document).ready(function () {
+    $('#btnCardFrontend').click(function () {
+        // Memeriksa apakah ada input yang kosong
+         swal('', 'Maaf Untuk Project Front end masih dalam tahap pembuatan, untuk contoh bisa di lihat web portofolio ini ', 'warning'); // Menampilkan peringatan menggunakan sweetalert
+        return false; // Menghentikan pengiriman form jika ada input yang kosong
+       
+    });
+});
+$(document).ready(function () {
+    $('#btnFotografi').click(function () {
+        // Memeriksa apakah ada input yang kosong
+         swal('', 'Untuk saat ini link hasil foto belum saya cantum kan ', 'warning'); // Menampilkan peringatan menggunakan sweetalert
+        return false; // Menghentikan pengiriman form jika ada input yang kosong
+       
+    });
+});
+$(document).ready(function () {
+    $('#btnCardVideo').click(function () {
+        // Memeriksa apakah ada input yang kosong
+         swal('', 'Untuk saat ini link hasil video belum saya cantumkan, kalau tidak keberatan bisa di lihat di akun tik tok saya di bagian footer ada beberapa sempel hasil karya video saya', 'warning'); // Menampilkan peringatan menggunakan sweetalert
         return false; // Menghentikan pengiriman form jika ada input yang kosong
        
     });
