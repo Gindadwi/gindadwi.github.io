5b8b3dceaf69c064c696b29d7b911a5897603114
Gindadwi <gindadwi144@gmail.com>
2024-04-02 09:27:30 +0700

    redirect wa & pop up

--- java.js
+++ java.js
@@ -1,11 +1,11 @@
 // Fungsi untuk mengirim pesan WhatsApp
 function sendMessage() {
-    const name = document.getElementById("name").value;
+    const name = document.getElementById ("name").value;
     const email = document.getElementById("email").value;
     const Message = document.getElementById("Message").value;
  
 
-    const url = "https://api.whatsapp.com/send?phone=6281228900185&text=%0Aassalamualaikum.%0A%0Anama%20saya%20* " + name + " *%0Aemail%20*" + email + "*%0A%0A*" + Message + "*"
+    const url = "https://api.whatsapp.com/send?phone=6281228900185&text=%0Aassalamualaikum.%0A%0Anama%20saya%20 " + name + " %0Aemail%20*" + email + "*%0A%0A" + Message + ""
 
     window.open(url, "_blank");
 }
